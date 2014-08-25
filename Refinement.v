f61dc9e911bc0e22782d152082a14dc47519198d
Adam Chlipala <adam@chlipala.net>
2014-08-25 17:32:05 -0400

    pick1

--- Refinement.v
+++ Refinement.v
@@ -1,4 +1,4 @@
-Require Import List Process Eqdep_dec.
+Require Import List Permutation Process Eqdep_dec.
 
 
 Ltac inv H := inversion H; clear H; subst;
@@ -224,6 +224,15 @@ Section Refinement.
     eapply interleave_reassoc2 in H7; try apply H5; firstorder eauto.
   Qed.
 
+  Lemma Parallel_cong : forall p q p' q',
+    refines (chs := chs) p p'
+    -> refines q q'
+    -> refines (p || q) (p' || q').
+  Proof.
+    unfold refines; intros.
+    inv H1; eauto.
+  Qed.
+
   Lemma procsD_app1 : forall ps2 ps1,
     refines (procsD (ps1 ++ ps2)%list) (Parallel (procsD ps1) (procsD ps2)).
   Proof.
@@ -346,6 +355,552 @@ Section Refinement.
 
     apply interleave_nil1 in H7; subst.
     eauto.
-  Qed.    
+  Qed.
+
+
+  (** * One step of a refinement proof between two normalizes processes *)
+
+  Open Scope list_scope.
+
+  Lemma refines_refl : forall p, refines (chs := chs) p p.
+  Proof.
+    firstorder.
+  Qed.
+
+  (* Recharacterize when a process system steps. *)
+  Lemma whenStep : forall ps tr, traceOf (procsD ps) tr
+    -> (* Trace ends *)
+    tr = nil
+
+    \/ (* Send *)
+    (exists tr' P ch v k ps', Permutation ps ({| Chans := P; Code := NSend ch v k |} :: ps')
+      /\ tr = (Send, existT _ ch v) :: tr'
+      /\ P (Send, ch)
+      /\ traceOf (procsD ({| Chans := P;
+                             Code := k |} :: ps')) tr')
+
+    \/ (* Recv *)
+    (exists tr' P ch v k ps', Permutation ps ({| Chans := P; Code := NRecv ch k |} :: ps')
+      /\ tr = (Recv, existT _ ch v) :: tr'
+      /\ P (Recv, ch)
+      /\ traceOf (procsD ({| Chans := P;
+                             Code := k v |} :: ps')) tr')
+
+    \/ (* Rendezvous *)
+    (exists P1 P2 ch v k1 k2 ps',
+       Permutation ps ({| Chans := P1; Code := NSend ch v k1 |}
+                         :: {| Chans := P2; Code := NRecv ch k2 |} :: ps')
+       /\ P1 (Send, ch)
+       /\ P2 (Recv, ch)
+       /\ traceOf (procsD ({| Chans := P1;
+                              Code := k1 |}
+                             :: {| Chans := P2;
+                                   Code := k2 v |}
+                             :: ps')) tr).
+  Proof.
+    induction ps; simpl; intuition.
+
+    inv H; eauto.
+
+    inv H; eauto.
+    unfold procD in H2; simpl in H2.
+    inv H2; eauto.
+
+    Focus 2.
+    apply interleave_nil1 in H5; subst.
+    apply IHps in H3; clear IHps; firstorder.
+    
+    right; left.
+    repeat esplit.
+    2: eauto.
+    eapply perm_trans; [ | eapply perm_swap ]; eauto.
+    eauto.
+    inv H2; eauto.
+
+    do 2 right; left.
+    repeat esplit.
+    2: eauto.
+    eapply perm_trans; [ | eapply perm_swap ]; eauto.
+    eauto.
+    inv H2; eauto.
+
+    do 3 right.
+    repeat esplit.
+    eapply perm_trans; [ | eapply perm_skip; eapply perm_swap ].
+    eapply perm_trans; [ | eapply perm_swap ]; eauto.
+    eauto.
+    eauto.
+    inv H2; eauto.
+    inv H6; eauto.
+
+    destruct H5.
+
+    (* IntNil1 *)
+
+    apply IHps in H3; clear IHps; firstorder.
+
+    right; left.
+    repeat esplit.
+    2: eauto.
+    eapply perm_trans; [ | eapply perm_swap ]; eauto.
+    eauto.
+    inv H3; eauto.
+
+    do 2 right; left.
+    repeat esplit.
+    2: eauto.
+    eapply perm_trans; [ | eapply perm_swap ]; eauto.
+    eauto.
+    inv H3; eauto.
+
+    do 3 right.
+    repeat esplit.
+    eapply perm_trans; [ | eapply perm_skip; eapply perm_swap ].
+    eapply perm_trans; [ | eapply perm_swap ]; eauto.
+    eauto.
+    eauto.
+    inv H3; eauto.
+    inv H8; eauto.
+
+    (* IntNil2 *)
+
+    destruct a; simpl in *.
+    destruct Code0; rewrite expand_proc0D in *; simpl in *.
+
+    inv H1; eauto.
+    right; left.
+    repeat esplit; eauto.
+    inv H6.
+    eauto.
+    unfold procD; simpl.
+    inv H6; eauto.
+
+    inv H1; eauto.
+    do 2 right; left.
+    repeat esplit; eauto.
+    inv H6.
+    eauto.
+    unfold procD; simpl.
+    inv H6; eauto.
+
+    inv H1; eauto.
+
+    (* IntCons1 *)
+
+    destruct a; simpl in *.
+    destruct Code0; rewrite expand_proc0D in *; simpl in *.
+
+    inv H1; eauto.
+    right; left.
+    repeat esplit; eauto.    inv H6.
+    eauto.
+    unfold procD; simpl.
+    inv H6; eauto.
+
+    inv H1; eauto.
+    do 2 right; left.
+    repeat esplit; eauto.
+    inv H6.
+    unfold procD; simpl.
+    eauto.
+    inv H6; eauto.
+    econstructor.
+    econstructor; eauto.
+    eauto.
+    eauto.
+
+    inv H1; eauto.
+
+    (* IntCons2 *)
+
+    apply IHps in H3; clear IHps; firstorder; try congruence.
+    
+    inv H0.
+    right; left.
+    repeat esplit.
+    eapply perm_trans; [ | eapply perm_swap ]; eauto.
+    eauto.
+    simpl.
+    inv H3; eauto.
+    Focus 2.
+    apply interleave_nil2 in H5; subst; eauto.
+    econstructor.
+    apply TrDone.
+    2: eauto.
+    econstructor.
+    econstructor.
+    eauto.
+    eauto.
+    apply TrDone.
+    auto.
+
+    apply Parallel_assoc2.
+    eapply Parallel_cong.
+    apply Parallel_comm.
+    apply refines_refl.
+    apply Parallel_assoc1.
+    econstructor.
+    econstructor; eauto.
+    econstructor; eauto.
+    eauto.
+
+    inv H0.
+    do 2 right; left.
+    repeat esplit.
+    eapply perm_trans; [ | eapply perm_swap ]; eauto.
+    eauto.
+    simpl.
+    inv H3; eauto.
+    Focus 2.
+    apply interleave_nil2 in H5; subst; eauto.
+    econstructor.
+    apply TrDone.
+    2: eauto.
+    econstructor.
+    econstructor.
+    eauto.
+    eauto.
+    apply TrDone.
+    auto.
+
+    apply Parallel_assoc2.
+    eapply Parallel_cong.
+    apply Parallel_comm.
+    apply refines_refl.
+    apply Parallel_assoc1.
+    econstructor.
+    econstructor; eauto.
+    econstructor; eauto.
+    eauto.
+
+    do 3 right.
+    repeat esplit.
+    eapply perm_trans; [ | eapply perm_skip; eapply perm_swap ].
+    eapply perm_trans; [ | eapply perm_swap ]; eauto.
+    eauto.
+    eauto.
+    simpl.
+    apply Parallel_assoc2.
+    apply Parallel_comm.
+    apply Parallel_assoc1.
+    eapply Parallel_cong.
+    apply refines_refl.
+    apply Parallel_comm.
+    econstructor.
+    econstructor; eauto.
+    eapply Parallel_assoc1.
+    eassumption.
+    eauto.
+
+    (* IntMatch1 *)
+
+    apply IHps in H3; clear IHps; firstorder; try congruence.
+
+    inv H0.
+    destruct a; simpl in *.
+    destruct Code0; rewrite expand_proc0D in *; simpl in *.
+
+    inv H1; eauto.
+    do 3 right.
+    inv H6.
+    repeat esplit.
+    eauto.
+    eauto.
+    eauto.
+    econstructor.
+    econstructor; eauto.
+    eauto.
+    eauto.
+
+    inv H1.
+    inv H1.
+
+    do 3 right.
+    repeat esplit.
+    eapply perm_trans; [ | eapply perm_skip; eapply perm_swap ].
+    eapply perm_trans; [ | eapply perm_swap ]; eauto.    
+    eauto.
+    eauto.
+    simpl.
+    apply Parallel_assoc2.
+    apply Parallel_comm.
+    apply Parallel_assoc1.
+    eapply Parallel_cong.
+    apply refines_refl.
+    apply Parallel_comm.
+    econstructor.
+    econstructor; eauto.
+    eapply Parallel_assoc1.
+    eassumption.
+    eauto.
+
+    (* IntMatch2 *)
+
+    apply IHps in H3; clear IHps; firstorder; try congruence.
+
+    inv H0.
+    destruct a; simpl in *.
+    destruct Code0; rewrite expand_proc0D in *; simpl in *.
+
+    inv H1; eauto.
+
+    inversion H1; clear H1.
+    destruct H8.
+    destruct H10.
+    apply (inj_pair2_eq_dec _ string_dec) in H7; subst.
+    apply (inj_pair2_eq_dec _ string_dec) in H9; subst.
+    do 3 right.
+    inv H6.
+    repeat esplit.
+    eapply perm_trans; [ | apply perm_swap ].
+    eauto.
+    eauto.
+    eauto.
+    eapply Parallel_cong.
+    apply refines_refl.
+    apply Parallel_comm.
+    apply Parallel_assoc2.
+    econstructor.
+    eauto.
+    econstructor; eauto.
+    eauto using interleave_sym.
+
+    inv H1.
+
+    do 3 right.
+    repeat esplit.
+    eapply perm_trans; [ | eapply perm_skip; eapply perm_swap ].
+    eapply perm_trans; [ | eapply perm_swap ]; eauto.
+    eauto.
+    eauto.
+    simpl.
+    apply Parallel_assoc2.
+    apply Parallel_comm.
+    apply Parallel_assoc1.
+    eapply Parallel_cong.
+    apply refines_refl.
+    apply Parallel_comm.
+    econstructor.
+    econstructor; eauto.
+    eapply Parallel_assoc1.
+    eassumption.
+    eauto.
+  Qed.
+
+  Theorem Permutation_traceOf : forall ps1 ps2, Permutation ps1 ps2
+    -> forall tr, traceOf (procsD ps1) tr
+      -> traceOf (procsD ps2) tr.
+  Proof.
+    induction 1; simpl; intuition.
+
+    inv H0; eauto.
+
+    apply Parallel_assoc2.
+    eapply Parallel_cong.
+    apply Parallel_comm.
+    apply refines_refl.
+    apply Parallel_assoc1; auto.
+  Qed.
+
+  Theorem oneStep' : forall ps2 ps1,
+    (forall P1 ch v k1 ps1', Permutation ps1 ({| Chans := P1; Code := NSend ch v k1 |} :: ps1')
+       -> P1 (Send, ch)
+       -> exists P2 k2 ps2', Permutation ps2 ({| Chans := P2; Code := NSend ch v k2 |} :: ps2')
+                             /\ P2 (Send, ch)
+                             /\ refines (procsD ({| Chans := P1; Code := k1 |} :: ps1'))
+                                        (procsD ({| Chans := P2; Code := k2 |} :: ps2')))
+    -> (forall P1 ch k1 ps1', Permutation ps1 ({| Chans := P1; Code := NRecv ch k1 |} :: ps1')
+       -> P1 (Recv, ch)
+       -> exists P2 k2 ps2', Permutation ps2 ({| Chans := P2; Code := NRecv ch k2 |} :: ps2')
+                             /\ P2 (Recv, ch)
+                             /\ forall v, refines (procsD ({| Chans := P1; Code := k1 v |} :: ps1'))
+                                                  (procsD ({| Chans := P2; Code := k2 v |} :: ps2')))
+    -> (forall P1 P2 ch v k1 k2 ps', Permutation ps1 ({| Chans := P1; Code := NSend ch v k1 |}
+                                                     :: {| Chans := P2; Code := NRecv ch k2 |}
+                                                     :: ps')
+       -> P1 (Send, ch)
+       -> P2 (Recv, ch)
+       -> refines (procsD ({| Chans := P1; Code := k1 |}
+                             :: {| Chans := P2; Code := k2 v |}
+                             :: ps')) (procsD ps2))
+    -> refines (procsD ps1) (procsD ps2).
+  Proof.
+    intros; do 2 intro.
+    apply whenStep in H2; firstorder; subst; eauto.
+
+    specialize (H _ _ _ _ _ H2); firstorder.
+    eapply Permutation_traceOf.
+    apply Permutation_sym; eassumption.
+    simpl.
+    assert (traceOf (procD {| Chans := x5; Code := x6 |} || procsD x7) x) by eauto.
+    inv H7; eauto.
+    inv H10; eauto.
+    econstructor.
+    econstructor.
+    rewrite expand_proc0D; simpl.
+    econstructor.
+    eauto.
+    eauto.
+    eauto.
+    eauto.
+    apply interleave_nil1 in H13; subst.
+    econstructor.
+    econstructor.
+    rewrite expand_proc0D; simpl.
+    econstructor.
+    eauto.
+    eauto.
+    eauto.
+    eauto.
+    econstructor.
+    econstructor.
+    rewrite expand_proc0D; simpl.
+    econstructor.
+    eauto.
+    eauto.
+    eauto.
+    eauto.
+
+    specialize (H0 _ _ _ _ H2); firstorder.
+    eapply Permutation_traceOf.
+    apply Permutation_sym; eassumption.
+    simpl.
+    assert (traceOf (procD {| Chans := x0; Code := x3 x2 |} || procsD x4) x) by eauto.
+    apply H6 in H7.
+    inv H7; eauto.
+    inv H10; eauto.
+    econstructor.
+    econstructor.
+    rewrite expand_proc0D; simpl.
+    econstructor.
+    eauto.
+    eauto.
+    eauto.
+    eauto.
+    apply interleave_nil1 in H13; subst.
+    econstructor.
+    econstructor.
+    rewrite expand_proc0D; simpl.
+    econstructor.
+    eauto.
+    eauto.
+    eauto.
+    eauto.
+    econstructor.
+    econstructor.
+    rewrite expand_proc0D; simpl.
+    econstructor.
+    eauto.
+    eauto.
+    eauto.
+    eauto.
+
+    specialize (H1 _ _ _ _ _ _ _ H2); firstorder.
+  Qed.
+
+  (* Easier statement to work with *)
+
+  Inductive pick1 {A : Type} : list A -> A -> list A -> Prop :=
+  | Fst1 : forall x ls, pick1 (x :: ls) x ls
+  | More1 : forall x y ls ls', pick1 ls y ls' -> pick1 (x :: ls) y (x :: ls').
+
+  Hint Constructors pick1.
+
+  Theorem pick1_Permutation : forall A (ls : list A) x ls',
+    pick1 ls x ls'
+    -> Permutation ls (x :: ls').
+  Proof.
+    induction 1; eauto.
+    eapply perm_trans; [ | apply perm_swap ].
+    eauto.
+  Qed.
+
+  Lemma Permutation_pick1' : forall A x (ls : list A) ls',
+    Permutation ls ls'
+    -> forall ls'', pick1 ls x ls''
+      -> exists ls''', Permutation ls'' ls''' /\ pick1 ls' x ls'''.
+  Proof.
+    induction 1; intuition (subst; eauto); try congruence.
+
+    inv H0; eauto.
+    apply IHPermutation in H5; firstorder eauto.
+
+    inv H; eauto.
+    inv H4; eauto.
+    do 2 esplit; [ | eauto ].
+    apply perm_swap.
+
+    apply IHPermutation1 in H1; firstorder.
+    apply IHPermutation2 in H2; firstorder.
+    do 2 esplit; [ | eauto ].
+    eauto using perm_trans.
+  Qed.
+
+  Theorem Permutation_pick1 : forall A (ls : list A) x ls',
+    Permutation ls (x :: ls')
+    -> exists ls'', Permutation ls' ls'' /\ pick1 ls x ls''.
+  Proof.
+    intros.
+    assert (pick1 (x :: ls') x ls') by auto.
+    eapply Permutation_pick1' in H0.
+    2: apply Permutation_sym; eassumption.
+    eauto.
+  Qed.
+
+  Theorem oneStep : forall ps2 ps1,
+    (forall P1 ch v k1 ps1', pick1 ps1 {| Chans := P1; Code := NSend ch v k1 |} ps1'
+       -> P1 (Send, ch)
+       -> exists P2 k2 ps2', pick1 ps2 {| Chans := P2; Code := NSend ch v k2 |} ps2'
+                             /\ P2 (Send, ch)
+                             /\ refines (procsD ({| Chans := P1; Code := k1 |} :: ps1'))
+                                        (procsD ({| Chans := P2; Code := k2 |} :: ps2')))
+    -> (forall P1 ch k1 ps1', pick1 ps1 {| Chans := P1; Code := NRecv ch k1 |} ps1'
+       -> P1 (Recv, ch)
+       -> exists P2 k2 ps2', pick1 ps2 {| Chans := P2; Code := NRecv ch k2 |} ps2'
+                             /\ P2 (Recv, ch)
+                             /\ forall v, refines (procsD ({| Chans := P1; Code := k1 v |} :: ps1'))
+                                                  (procsD ({| Chans := P2; Code := k2 v |} :: ps2')))
+    -> (forall P1 P2 ch v k1 k2 ps', Permutation ps1 ({| Chans := P1; Code := NSend ch v k1 |}
+                                                     :: {| Chans := P2; Code := NRecv ch k2 |}
+                                                     :: ps')
+       -> P1 (Send, ch)
+       -> P2 (Recv, ch)
+       -> refines (procsD ({| Chans := P1; Code := k1 |}
+                             :: {| Chans := P2; Code := k2 v |}
+                             :: ps')) (procsD ps2))
+    -> refines (procsD ps1) (procsD ps2).
+  Proof.
+    intros; eapply oneStep'; eauto; intros.
+
+    apply Permutation_pick1 in H2; firstorder.
+    specialize (H _ _ _ _ _ H4); firstorder.
+    repeat esplit.
+    eauto using pick1_Permutation.
+    eauto.
+    do 2 intro.
+    apply H6.
+    inv H7; eauto.
+    econstructor.
+    eauto.
+    eapply Permutation_traceOf.
+    eassumption.
+    eauto.
+    auto.
+
+    apply Permutation_pick1 in H2; firstorder.
+    specialize (H0 _ _ _ _ H4); firstorder.
+    repeat esplit.
+    eauto using pick1_Permutation.
+    eauto.
+    do 3 intro.
+    apply H6.
+    inv H7; eauto.
+    econstructor.
+    eauto.
+    eapply Permutation_traceOf.
+    eassumption.
+    eauto.
+    auto.
+  Qed.
 
 End Refinement.
