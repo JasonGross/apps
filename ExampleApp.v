6c412f4ca69cc89da3e38eed22af3b6d0374e9eb
Peng Wang <wangp.thu@gmail.com>
2014-09-15 18:05:01 -0400

    Writing ExampleApp.v

--- ExampleApp.v
+++ ExampleApp.v
@@ -1,1 +1,15 @@
+(* An example adapted from OKWS, a web server with privilege separation 
+   Reference paper: 
+   Maxwell Krohn, Building Secure High-Performance Web Services with OKWS *)
+
 Require Import Arith Process Refinement ModelCheck.
+
+Definition chs : channels := fun _ => nat.
+
+Module okd.
+
+  Definition policy (pr : process chs) : process chs := ##[(Recv, "80"), (Send, "80"), (Send, "svc1")], pr.
+
+End okd.
+
+Module svc1.
