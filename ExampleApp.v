be4fcf04fbf28e64054027432aac220bf3427497
Peng Wang <wangp.thu@gmail.com>
2014-09-15 22:31:05 -0400

    Wrote an example web server based on the OKWS paper in ExampleApp.v

--- ExampleApp.v
+++ ExampleApp.v
@@ -1,15 +1,74 @@
+Set Implicit Arguments.
+Require Import Arith Process Refinement ModelCheck.
+
+(* Pi-calculus *)
+CoInductive process :=
+| DoSend (ch : channel) T (v : T) (k : process)
+| DoRecv (ch : channel) T (k : T -> process)
+| Parallel (pr1 pr2 : process)
+| CreateChan (k : channel -> process)
+| RepeatedSpawn (pr : process)
+| Restrict (which : direction * channel -> Prop) (k : process)
+| Done.
+
+Require Import String List.
+Notation "#! [ ch , v ] , k" := (DoSend ch v k) (at level 100).
+Notation "#? [ ch , x ] , k" := (DoRecv ch (fun x => k)) (at level 100).
+Notation "## [ x1 , .. , xN ] , k" := (Restrict (fun p => In p (cons x1 (.. (cons xN nil) ..))) k) (at level 100).
+Infix "||" := Parallel.
+Notation "#*" := RepeatedSpawn.
+Notation "#@ ch , k" := (CreateChan (fun ch => k)) (at level 100).
+Import ListNotations.
+
 (* An example adapted from OKWS, a web server with privilege separation 
    Reference paper: 
    Maxwell Krohn, Building Secure High-Performance Web Services with OKWS *)
 
-Require Import Arith Process Refinement ModelCheck.
+Definition HTTP_PORT := "80".
 
-Definition chs : channels := fun _ => nat.
+Definition HTTP_404 (sock : channel) := #![sock, "Response 404"], Done.
 
-Module okd.
+Definition Dispatcher := string -> option channel.
 
-  Definition policy (pr : process chs) : process chs := ##[(Recv, "80"), (Send, "80"), (Send, "svc1")], pr.
+(* The dispatching deamon, which listens to port 80 and dispatches incoming HTTP requests according to the first line of the request *)
+Definition okd (dispatcher : Dispatcher) := 
+  #?[HTTP_PORT, sock], #?[sock, reqpath], 
+  match dispatcher reqpath with
+    | Some svc => #![svc, reqpath], #![svc, sock], Done
+    | None => HTTP_404 sock
+  end.
 
-End okd.
+Definition Service := string -> channel -> process.
 
-Module svc1.
+(* The launcher, which launches okd and a collection of services, and sets up dispatching rules *)
+Section okld.
+
+  Variable make_dispatcher : list (string * channel) -> Dispatcher.
+
+  (* A helper *)
+  Definition start_svc (svc : Service) ch := #?[ch, reqpath], #?[ch, sock], svc reqpath sock.
+
+  (* An example launcher for a fixed number of services *)
+  Definition okld3 ptrn1 svc1 ptrn2 svc2 ptrn3 svc3 :=
+    #@ ch1, #@ ch2, #@ ch3,
+    let dispatcher := make_dispatcher [(ptrn1, ch1); (ptrn2, ch2); (ptrn3, ch3)] in
+    okd dispatcher ||
+    start_svc svc1 ch1 ||
+    start_svc svc2 ch2 ||
+    start_svc svc3 ch3.
+
+  Fixpoint CreateChannels n (f : list channel -> process) : process :=
+    match n with
+      | 0 => f []
+      | S n'=> CreateChannels n' (fun chs => #@ ch, f (ch :: chs))
+    end.
+
+  Definition fold_left2 A B C (f : A -> B -> C -> A) ls1 ls2 a := fold_left (fun a x => f a (fst x) (snd x)) (combine ls1 ls2) a.
+
+  (* A more general launcher for an arbitrary list of services *)
+  Definition okld (svcs : list (string * Service)) := CreateChannels (length svcs) (fun chans =>
+    let (patterns, svcs) := split svcs in
+    let dispatcher := make_dispatcher (combine patterns chans) in
+    fold_left2 (fun pr ch svc => pr || start_svc svc ch) chans svcs (okd dispatcher)).
+
+End okld.
