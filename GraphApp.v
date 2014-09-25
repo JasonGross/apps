e5b736071be720d2758bab65f79d2f79057356fb
Anders Kaseorg <andersk@mit.edu>
2014-09-25 03:29:42 -0400

    GraphApp: go back to hlist, add nice notations

--- GraphApp.v
+++ GraphApp.v
@@ -1,51 +1,76 @@
-Inductive sum' : list Type -> Type :=
-| SHead {t ts} : t -> sum' (t :: ts)
-| STail {t ts} : sum' ts -> sum' (t :: ts).
+Section hlist.
+  Context {a : Type} {b : a -> Type}.
+
+  Inductive hlist : list a -> Type :=
+  | HNil : hlist nil
+  | HCons {x : a} {ls : list a} : b x -> hlist ls -> hlist (x :: ls).
+
+  Definition huncons {x xs} (h : hlist (x :: xs)) :=
+    let 'HCons _ _ y h' := h in (y, h').
+
+  Definition match_hnil {r} (x : r) (_ : hlist nil) := x.
+
+  Definition match_hcons {x xs r} (f : b x -> hlist xs -> r) h :=
+    let (y, h') := huncons h in f y h'.
+End hlist.
+
+Arguments hlist {a} b _.
 
 
 Section process.
-  (* A process reads from one input channel and writes to one output
-  channel. *)
-
-  Context {input output : Type}.
+  Context {var : Type -> Type} {input : Type}.
 
   CoInductive process :=
   | DoRecv : (input -> process) -> process
-  | DoSend : output -> process -> process.
-
-  (* mapProc f generates one output for each input. *)
-  CoFixpoint mapProc f :=
-    DoRecv (fun x => DoSend (f x) (mapProc f)).
+  | DoSend {output} : var output -> output -> process -> process.
 End process.
 
 Arguments process : default implicits.
 
+Notation "'recv' x , k" := (DoRecv (fun x => k)) (at level 200, right associativity).
+Notation "'send' x 'to' ch , k" := (DoSend ch x k) (at level 200, right associativity).
+
 
 Section network.
-  (* PHOAS-like graph representation. *)
-
   Context {var : Type -> Type}.
 
-  Inductive network : list Type -> Type :=
-  | Empty : network nil
-  | Var {input} :
-      var input ->
-      forall {inputs}, network inputs -> network (input :: inputs)
-  | Mu {input inputs} :
-      (var input -> network (input :: inputs)) -> network inputs
-  | Node {input outputs} :
-      process input (sum' outputs) ->
-      network outputs ->
-      forall {inputs}, network inputs -> network (input :: inputs).
+  Inductive network : Type -> Type :=
+  | Var {input} : var input -> network input
+  | Mu {inputs input} :
+      (hlist var inputs -> hlist network inputs * network input) ->
+      network input
+  | Node {input} :
+      process var input ->
+      network input.
 End network.
 
 Arguments network : default implicits.
 
+Notation "'net' v0 .. v1 => n0 ; .. ; n1 'in' n" :=
+  (Mu (match_hcons (fun v0 => .. (match_hcons (fun v1 => match_hnil (HCons n0 (.. (HCons n1 HNil) ..), n))) ..)))
+    (at level 200, v0 binder, v1 binder, right associativity).
 
-(* Ping-pong example. *)
-Definition pingpong var t : network var (t :: nil) :=
-  Mu (fun a =>
-        Mu (fun b =>
-              Node (mapProc SHead) (Var a Empty)
-                   (Node (mapProc SHead) (Var b Empty)
-                         (Var a Empty)))).
+
+Section Demo.
+
+  Context {var : Type -> Type}.
+
+  (* Ping-pong example. *)
+  Definition pingpong t : network var t :=
+    net a b =>
+      Node (cofix loop := recv x, send x to b, loop);
+      Node (cofix loop := recv x, send x to a, loop)
+    in Var a.
+
+  (* “Game” example from meeting. *)
+  Section Game.
+    Require Import Ascii.
+
+    Variable video : var (nat * nat * bool).
+
+    Definition game : network var (ascii : Type) :=
+      Node ((cofix loop n := recv _, send (n, n, true) to video, loop (n + 1))
+              0).
+  End Game.
+
+End Demo.
