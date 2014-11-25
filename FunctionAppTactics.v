830ef87e1a61e112b0e607b18f93a9bddbc5f754
Jason Gross <jgross@mit.edu>
2014-11-25 18:25:06 -0500

    Minor cleanup

--- FunctionAppTactics.v
+++ FunctionAppTactics.v
@@ -8,14 +8,6 @@ Ltac emptiesStack_t' loop_eta loop_pattern :=
   idtac;
   match goal with
     | _ => intro
-    | [ |- exists p : _, emptiesStack (stackTransition _ _) p /\ _ ] => unfold stackTransition; simpl
-    | [ |- exists p : _, emptiesStack (stackPush _ _, Step _) p /\ _ ] => apply emptiesStackPush_ex
-    | [ |- exists p : _, emptiesStack (stackLift _ _, _) p /\ _ ] => apply emptiesStackLift_ex
-    | [ |- exists p : _, emptiesStack (stackDone, _) p /\ _ ] => apply emptiesStackDone_ex
-    | [ |- exists q : _, emptiesStack (_, ?p) q /\ _ ]
-      => let p' := head p in
-         constr_eq p' loop_pattern;
-           rewrite loop_eta; simpl
     | [ |- emptiesStack (stackTransition _ _) ?e ] => unfold stackTransition; simpl
     | [ |- emptiesStack (stackPush _ _, Step _) ?e ] => eapply emptiesStackPush
     | [ |- emptiesStack (stackLift _ _, _) ?e ] => apply emptiesStackLift
@@ -40,14 +32,9 @@ Ltac emptiesStackForever_t' loopGood' inputT loop_eta loop_pattern tac :=
     | [ H : inputT |- _ ] => destruct H
     | _ => progress unfold stackTransition, compose
     | _ => progress tac
-    | [ |- exists p : _, emptiesStack (?instrs, _) p /\ _ ]
-      => (lazymatch instrs with
-         (*| appcontext[stackPush] => emptiesStack_t' loop_eta loop_pattern*)
-         | _ => (repeat (eexists; split; [ clear loopGood' | try solve [ apply loopGood' ] ]);
-                 match goal with
-                   | [ |- emptiesStack _ _ ] => emptiesStack_t loop_eta loop_pattern
-                 end)
-          end)
+    | [ |- exists p : _, emptiesStack _ p /\ _ ]
+      => (eexists; split; [ clear loopGood' | try solve [ apply loopGood' ] ];
+          emptiesStack_t loop_eta loop_pattern)
     | [ |- emptiesStackForever ?p ]
       => let p' := head p in
          constr_eq p' loop_pattern;
